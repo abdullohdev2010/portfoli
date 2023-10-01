9af30c029cac43eb0852ab0a12536c829df59f0f
Akbarov Abdulloh <112395920+abdullohdev2010@users.noreply.github.com>
2023-10-01 14:28:27 +0500

    qo'shish
    ++

--- README.md
+++
@@ -1,2 +0,0 @@
-# portfoli
-My portfoli
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const header = document.querySelector('header');
-
+const row = document.querySelectorAll('.row')
 window.addEventListener ('scroll', function() {
     header.classList.toggle ('sticky', window.scrollY > 120)
 })
